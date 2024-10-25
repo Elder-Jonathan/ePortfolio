7cd6802353dcd38151ae957a18ec62bbe27f2c71
Elder-Jonathan <elder-jonathan@outlook.com>
2024-10-25 09:18:45 -0500

    Add directory for M1 Activity 1 Files
    - Reorganized folder structure.
    - M1 now has two subdirectories. This is Activity 1 Files.

---
+++ CPSC 1210 - Intro to Computer Science I/Module 1/Activity 1 Files/CourseInfo.java
@@ -0,0 +1,23 @@
+/**
+*
+* Activity 1 Part B.
+* Name: Jonathan Elder
+* Course Info: CPSC 1210 Intro to Computer Science
+* Today's Date: 5/25/2022
+*
+*/
+public class CourseInfo {
+   /**
+   * Prints three lines of course info.
+   * @param args Command line arguments - not used.
+   */
+   public static void main(String[] args) {
+      System.out.println("This course provides an introduction to Java and");
+      System.out.println("object-oriented programming.");
+      
+      System.out.println(); // Line Break.
+      
+      System.out.println("The course also introduces the basics of " 
+         + "software development.");
+   }
+}
