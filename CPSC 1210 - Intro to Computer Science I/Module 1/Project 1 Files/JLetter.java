0dfb22d6a587ee80b913d15bf51d8c97842301c8
Elder-Jonathan <elder-jonathan@outlook.com>
2024-10-25 09:29:46 -0500

    Final versions for CPSC 1210 M1 Project 1 Files
    - CPSC 1210 Module 1 Project 1 Auburn University.
    - Final versions of MyLifeGoals and JLetter classes submitted for course credit.
    - This commit includes the .java and compiled .class files.

---
+++ CPSC 1210 - Intro to Computer Science I/Module 1/Project 1 Files/JLetter.java
@@ -0,0 +1,28 @@
+/**
+*
+* Project 1 Part 2.
+* Name: Jonathan Elder.
+* Course Info: CPSC 1210 Intro to Computer Science.
+* Today's Date: 5/28/2022.
+*
+*/
+public class JLetter 
+{
+    /**
+    * Prints the letter J as a large block letter using the word JAVA.
+    * @param args Command line arguments - not used.
+    * @author Jonathan Elder
+    */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ CPSC 1210 - Intro to Computer Science I/Module 1/Project 1 Files/MyLifeGoals.java
@@ -0,0 +1,27 @@
+/**
+*
+* Project 1 Part 1.
+* Name: Jonathan Elder.
+* Course Info: CPSC 1210 Intro to Computer Science.
+* Today's Date: 5/28/2022.
+*
+*/
+public class MyLifeGoals 
+{
+   /**
+   * Prints my short, medium, and long term goals.
+   * @param args Command line arguments - not used.
+   * @author Jonathan Elder
+   */
+   public static void main(String[] args) {
+      System.out.println("Jonathan Elder\n");
+      System.out.println("Short term life goals: I want to finish this course"
+         + " and my other summer course with the best possible grade.");
+      System.out.println("Medium term life goals: Finishing my degree path here"
+         + " at Auburn with good job prospects from"
+         + " multiple high tier employers.");
+      System.out.println("Long term life goals: Having years of success in my"
+         + " new career path. Being able to retire comfortably,"
+         + " without the concern of money.");
+   }
+}
