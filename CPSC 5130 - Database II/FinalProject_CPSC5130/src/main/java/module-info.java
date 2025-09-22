77ecbf00773496853d952f7416352e82e9a38a92
Elder-Jonathan <elder-jonathan@outlook.com>
2025-09-21 23:04:06 -0500

    Delete CPSC 5130 - Database II/FinalProject_CPSC5130 directory

--- CPSC 5130 - Database II/FinalProject_CPSC5130/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.finalproject_cpsc5130 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.finalproject_cpsc5130 to javafx.fxml;
-    exports com.example.finalproject_cpsc5130;
-}
